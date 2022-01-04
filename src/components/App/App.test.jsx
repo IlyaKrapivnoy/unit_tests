ecef0f37fbc9a57384710dbfc3b5107221caad7c
Kath <ilya.krapivnoy@gmail.com>
2022-01-04 13:18:49 +0200

    type in Searchbox works

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,16 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-// test('renders learn react link', () => {
-// render(<App />);
-// const linkElement = screen.getByText(/learn react/i);
-// expect(linkElement).toBeInTheDocument();
-// });
+import userEvent from '@testing-library/user-event';
 
 describe('App component', () => {
     it('App renders', () => {
         render(<App />);
 
         expect(screen.getByRole('list')).toBeInTheDocument();
+        expect(screen.getByText('Find course:')).toBeInTheDocument();
+    });
+
+    it('typing in Searchbox works', () => {
+        render(<App />);
+
+        expect(screen.queryByDisplayValue(/React/)).toBeNull();
+
+        userEvent.type(screen.getByRole('textbox'), 'React');
+        expect(screen.queryByDisplayValue(/React/)).toBeInTheDocument();
     });
 });
